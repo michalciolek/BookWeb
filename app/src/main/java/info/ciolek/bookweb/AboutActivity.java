8c42ca88a6e5f95db2b3552db266ccc738678b8e
michalciolek <michalciolek@gmail.com>
2015-04-14 21:20:44 +0200

    #02

--- app/src/main/java/info/ciolek/bookweb/AboutActivity.java
+++ app/src/main/java/info/ciolek/bookweb/AboutActivity.java
@@ -1,9 +1,11 @@
 package info.ciolek.bookweb;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class AboutActivity extends ActionBarActivity {
@@ -12,6 +14,15 @@ public class AboutActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        Button button = (Button) findViewById(R.id.ff);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
     @Override
